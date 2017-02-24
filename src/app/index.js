50eed91ac1ebbfc392430728276114ad6c06021e
shassaninia <xai.soft80@gmail.com>
2017-02-24 09:10:37 -0500

    setting initial state

--- src/app/index.js
+++ src/app/index.js
@@ -4,22 +4,33 @@ var ReactDOM = require('react-dom');
 
 //Create component react way
 var TodoComponent = React.createClass({
+
+    //Initialize state of TodoComponent
+    //When to use state and/or properties?  Many use in tandem
+    getInitialState:function()
+    {
+        return {
+            todos:['wash up','eat some cheese', 'take a nap']
+        }
+    },
+
     //html to return from componenet
     //This is the only required function
     render: function(){
         return (
-            <div>
-            
-            <p>String property: {this.props.msg}</p>
-            <p><strong>Chess Name: </strong>{this.props.cheese.name}</p>
-            <p><strong>Smell Factor: </strong>{this.props.cheese.smellFactor}</p>
-            <p><strong>Price: </strong>{this.props.cheese.price}</p>
+            <div id="todo-list">
+                <p>The busiest people have the most leisure</p>
+                <ul>
+                    <li>{this.state.todos[0]}</li>
+                    <li>{this.state.todos[1]}</li>
+                    <li>{this.state.todos[2]}</li>
+                </ul>
             </div>
         );
     }
 });
 
-var myCheese = {name:'Camembert',smellFactor:'Extreme pong',price:'3.50'};
+
 
 //Put component into html page
-ReactDOM.render(<TodoComponent msg="Hello" cheese={myCheese}/>,document.getElementById('todo-wrapper'));
+ReactDOM.render(<TodoComponent/>,document.getElementById('todo-wrapper'));
