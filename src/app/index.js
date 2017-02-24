c04221a47575f8efb9c4a084a17544e34509ab89
shassaninia <xai.soft80@gmail.com>
2017-02-24 14:36:06 -0500

    installed router and added about component

--- src/app/index.js
+++ src/app/index.js
@@ -2,10 +2,24 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 require('./css/index.css');
+
+import {Router, Route, browserHistory} from 'react-router';
+
 // Module requires
 var TodoItem = require('./todoItem');
 var AddItem = require('./addItem');
+var About = require('./about');
 
+var App = React.createClass({
+    render:function(){
+        return(
+            <Router history={browserHistory}>
+                <Route path={'/'} component={TodoComponent}></Route>
+                <Route path={'/about'} component={About}></Route>
+            </Router>
+        );
+    }
+});
 //Create component react way
 var TodoComponent = React.createClass({
 
@@ -82,4 +96,4 @@ var TodoComponent = React.createClass({
 
 
 //Put component into html page
-ReactDOM.render(<TodoComponent/>,document.getElementById('todo-wrapper'));
+ReactDOM.render(<App/>,document.getElementById('todo-wrapper'));
